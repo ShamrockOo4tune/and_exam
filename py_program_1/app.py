a301caafc99b03daf0c1c161d1d29d110d2571fe
Shamil Gumerov <shamusg12345@gmail.com>
2022-01-03 08:39:42 +0300

    add python app

---
+++ py_program_1/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+hello_message = "Hello World 1"
+@app.route('/')
+def index():
+    return hello_message
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
+
