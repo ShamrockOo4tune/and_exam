371c2110dd334b91a0c5d1e855c2c137d39d436b
Shamil Gumerov <shamusg12345@gmail.com>
2022-01-07 11:36:58 +0300

    add golang web app and GoDockerfile

---
+++ go_program_2/main.go
@@ -0,0 +1,10 @@
+package main
+import "github.com/gin-gonic/gin"
+
+const(hello_message = "Hello World 2")
+func main() {
+    router := gin.Default()
+    router.GET("/", func (c  *gin.Context) {c.String(200, hello_message)})
+    router.Run(":8080")
+}
+
